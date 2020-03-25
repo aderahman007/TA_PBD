ec4e16a878b9a494f1544e0209c38b19a9a73ec9
aderahman007 <aderahman9908@gmail.com>
2020-03-26 01:14:49 +0700

    Menambah Layout Tentang Ilmu Tajwid dan Tips Belajar Ilmu Tajwid

--- app/src/main/java/com/example/tapbd3/HomeFragment.java
+++ app/src/main/java/com/example/tapbd3/HomeFragment.java
@@ -1,8 +1,10 @@
 package com.example.tapbd3;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -20,12 +22,32 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-
+    private CardView home;
+    private CardView tips;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        home = v.findViewById(R.id.ttg_Tajwid);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TentangIlmuTajwidActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        tips = v.findViewById(R.id.tip_bljr_Tajwid);
+        tips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), TipsBelajarIlmuTajwidActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 
 }
